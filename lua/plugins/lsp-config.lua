071dcbc6143a368297635f959012444eb3c5c28f
Retr05041 <parker@cranfield.ca>
2024-01-30 22:57:04 -0800

    Fix Lsp Plugin & Add base servers

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,19 +3,35 @@
 return {
     {
         "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
         config = function() 
             require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
             require("mason-lspconfig").setup({
                 -- Specify which language servers you want installed
-                ensure_installed = { "lua_ls" }
+                ensure_installed = { "lua_ls", "bashls", "clangd", "cmake", "html", "cssls", "sqlls", "yamlls" }
             })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
             local lspconfig = require("lspconfig")
             -- Must do a setup for each installed language server
             lspconfig.lua_ls.setup {}
-
+						lspconfig.bashls.setup {}
+						lspconfig.clangd.setup {}
+						lspconfig.cmake.setup {}
+						lspconfig.html.setup {}
+						lspconfig.cssls.setup {}
+						lspconfig.sqlls.setup {}
+						lspconfig.yamlls.setup {}
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
         end
     }
 }
+
+
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,4 +1,4 @@
--- Custom keybinds
+-- Default Vim Configuration
 
 -- Copy
 vim.api.nvim_set_keymap('n', '<C-c>', '"+y<CR>', {noremap = true, silent = true})
@@ -11,3 +11,13 @@ vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', {noremap = true, silent = true})
 
 -- Undo
 vim.api.nvim_set_keymap('n', '<C-z>', ':u<CR>', {noremap = true, silent = true})
+
+-- Fast close
+vim.api.nvim_set_keymap('n', '<C-w>', ':q<CR>', {noremap = true, silent = true})
+
+-- Show line numbers
+vim.opt.number = true
+
+-- Tab spacing
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
