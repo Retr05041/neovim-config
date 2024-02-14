142c4340163cb6ccc515d11f06ff6164a2e65fc2
Retr05041 <parker@cranfield.ca>
2024-02-13 21:42:41 -0800

    Added custom dashboard

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-f>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
     end,
   },
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,16 +2,16 @@
 vim.g.mapleader = " "
 
 -- Copy
-vim.api.nvim_set_keymap("n", "<leader>c", '"+y<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>c", '"+y<CR>', { noremap = true, silent = true })
 
 -- Paste
-vim.api.nvim_set_keymap("n", "<leader>v", '"+p<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>v", '"+p<CR>', { noremap = true, silent = true })
 
 -- Save
-vim.api.nvim_set_keymap("n", "<leader>s", ":w<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>s", ":w<CR>", { noremap = true, silent = true })
 
 -- Fast close
-vim.api.nvim_set_keymap("n", "<leader>w", ":q<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>w", ":q<CR>", { noremap = true, silent = true })
 
 -- Show line numbers
 vim.opt.number = true
